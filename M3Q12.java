cf47d89f4e4514d431c987c96ea2e921568518cc
SKRB <56062164+jerry39313683@users.noreply.github.com>
2019-12-25 15:46:44 +0800

    Create M3Q12.java

---
+++ M3Q12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class M3Q12{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n =scn.nextInt();
+        int m =scn.nextInt();
+        int f [] = new int [n];
+        int g [] = new int [m];
+        for(int i = 0; i<n ;i++){
+            f[i]=scn.nextInt();
+        }
+        for(int i = 0; i<m ; i++){
+            g[i]=scn.nextInt();
+        }
+        int j=0,k=0,count=0;
+        boolean flag = true;
+        for(int i = 0; i<n ; i++){
+            j=0;
+            flag=true;
+            k=0;
+            while(j<m){
+                if(f[i]==f[k]){
+                    count++;
+                    break;
+                }
+                j++;
+            }
+        }
+        System.out.println(count);
+    }
+}
