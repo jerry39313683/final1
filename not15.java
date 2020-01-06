3b935fa5aeff7f7112be422a7e60205e08c0c8c4
SKRB <56062164+jerry39313683@users.noreply.github.com>
2020-01-06 17:52:15 +0800

    Create not15.java

---
+++ not15.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class M3Q15{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int m1 = scn.nextInt();
+        int n2 = scn.nextInt();
+        int m2 = scn.nextInt();
+        int squ1 [][]=new int[n1][m1];
+        int squ2 [][]=new int[n2][m2];
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<m1;j++){
+                squ1[i][j]=scn.nextInt();
+            }
+        }
+        for(int i=0;i<n2;i++){
+            for(int j=0;j<m2;j++){
+                squ2[i][j]=scn.nextInt();
+            }
+        }
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<m1;j++){
+                for(k=0;k<)
+            }
+        }
+    }
+}
