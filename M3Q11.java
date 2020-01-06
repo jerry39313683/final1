2c8ec9df047dd866dc92ab5418b9e47c2f556859
SKRB <56062164+jerry39313683@users.noreply.github.com>
2020-01-06 16:51:15 +0800

    Create M3Q11.java

---
+++ M3Q11.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class M3Q11{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n =scn.nextInt();
+        int data[]= new int[n];
+        for(int i=0;i<n;i++){
+            data[i]=scn.nextInt();           
+        }
+        for(int i=0;i<n;i++){
+            int sum=data[i]; 
+            for(int j=i+1;j<n;j++){
+                sum=sum+data[j];
+                if(sum==0){
+                    for(int k=i;k<=j;k++){
+                        System.out.print(data[k]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+        }        
+    }
+}
